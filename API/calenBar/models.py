0de7485a08e88c20ce4477ef7ea8ad8ccf4bdf4e
Aaron Bergfeld <aaronbergfeld@gmail.com>
2023-04-01 13:32:10 -0400

    CAL-7 CAL-8 add task and calendar models

--- API/calenBar/models.py
+++ API/calenBar/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
 # Create your models here.
+
+class Task(models.Model):
+    title = models.CharField(max_length=200)
+    priority = models.IntegerField()
+    start_date = models.DateTimeField()
+    end_date = models.DateTimeField()
+    color = models.CharField(max_length=100)
+    description = models.TextField()
+    estimated_time = models.IntegerField()
+    completed = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.title
+
+class Calendar(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+    color_theme = models.CharField(max_length=100)
+    tasks = models.ManyToManyField(Task)
+
+    def __str__(self):
+        return self.title
